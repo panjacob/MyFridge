3626daac906caa6736410703fef0c6b0eabafbfe
panjacob <s19615@pjwstk.edu.pl>
2020-12-22 11:48:44 +0100

    Produkty ciag dalszy

--- fridge/models.py
+++ fridge/models.py
@@ -1,10 +1,6 @@
 from django.db.models import *
 
 
-# python manage.py makemigrations Fridge
-# python manage.py migrate
-
-
 class Type(Model):
     type = CharField(max_length=64, null=True)
 
@@ -17,11 +13,6 @@ class Product(Model):
     name = CharField(max_length=64, null=True)
     type = ForeignKey(Type, null=True, on_delete=CASCADE)
     img_name = CharField(max_length=64, null=True)
-    # Narazie nie potrzebne
-    # calories = IntegerField()
-    # carbohydrates = IntegerField()
-    # fat = IntegerField()
-    # protein = IntegerField()
     ammount = IntegerField()
     unit = ForeignKey(Unit, null=True, on_delete=CASCADE)
 
