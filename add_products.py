8348f917b156f32f7b1a50872998939dbb96be85
panjacob <s19615@pjwstk.edu.pl>
2021-01-07 16:38:34 +0100

    Naprawiony static

--- add_products.py
+++ add_products.py
@@ -3,18 +3,18 @@ from fridge.models import *
 
 import json
 
-with open('fridge/lib/products.json') as json_file:
+with open('fridge/static/products.json') as json_file:
     data = json.load(json_file)
 
 fridge, ok = Fridge.objects.get_or_create(id=0)
 
 for x in data:
-    print(x)
-    product, ok = Product.objects.get_or_create(name=x['name'], ammount=x['ammount'])
-    product.img_name = x['image_name']
-    typex, ok = Type.objects.get_or_create(type=x['type'])
-    unit, ok = Unit.objects.get_or_create(unit=x['unit'])
-    product.type = typex
-    product.unit = unit
-    product.save()
-    fridge_product, ok = FridgeProduct.objects.get_or_create(product=product, fridge=fridge)
+    if not Product.objects.get(name=x['name']).exists():
+        product, ok = Product.objects.get_or_create(name=x['name'], ammount=x['ammount'])
+        product.img_name = x['image_name']
+        typex, ok = Type.objects.get_or_create(type=x['type'])
+        unit, ok = Unit.objects.get_or_create(unit=x['unit'])
+        product.type = typex
+        product.unit = unit
+        product.save()
+        fridge_product, ok = FridgeProduct.objects.get_or_create(product=product, fridge=fridge)
--- myFridge/settings.py
+++ myFridge/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = '0lwm5hmqnufjee^ids(5qu*$_nxm0qh#^1z#df$3l!mtvvy3xv'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myFridge.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,16 +112,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "../fridge/static"),
 ]
 
 LOGIN_REDIRECT_URL = 'fridge-home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
