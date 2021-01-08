17b853f21a759d22f628356092c9215ce0b73dc5
panjacob <s19615@pjwstk.edu.pl>
2021-01-08 13:16:50 +0100

    Recipes w trakcie

--- add_products.py
+++ add_products.py
@@ -3,18 +3,37 @@ from fridge.models import *
 
 import json
 
-with open('fridge/static/products.json') as json_file:
-    data = json.load(json_file)
 
-fridge, ok = Fridge.objects.get_or_create(id=0)
+def add_products():
+    with open('fridge/static/products.json') as json_file:
+        data = json.load(json_file)
 
-for x in data:
-    if not Product.objects.filter(name=x['name']).exists():
-        print(x['name'])
-        product, ok = Product.objects.get_or_create(name=x['name'], ammount=x['ammount'])
+    fridge, ok = Fridge.objects.get_or_create(id=0)
+
+    for x in data:
+        # print(x['name'])
+        product, ok = Product.objects.get_or_create(name=x['name'])
         product.img_name = x['image_name']
         typex, ok = Type.objects.get_or_create(type=x['type'])
         unit, ok = Unit.objects.get_or_create(unit=x['unit'])
         product.type = typex
         product.unit = unit
+        product.amount = int(x['amount'])
         product.save()
+
+def add_recipes():
+    with open('fridge/static/recipes.json') as json_file:
+        data = json.load(json_file)
+
+    for x in data:
+        print(x['name'])
+        recipe, ok = Recipe.objects.get_or_create(name=x['name'])
+        recipe.img_name = x['image_name']
+        recipe.short_description =x['short_description']
+        recipe.long_description = x['long_description']
+        recipe.save()
+
+        print(x['products'])
+
+add_products()
+add_recipes()
--- fridge/models.py
+++ fridge/models.py
@@ -2,19 +2,19 @@ from django.db.models import *
 
 
 class Type(Model):
-    type = CharField(max_length=64, null=True)
+    type = CharField(max_length=64)
 
 
 class Unit(Model):
-    unit = CharField(max_length=64, null=True)
+    unit = CharField(max_length=64)
 
 
 class Product(Model):
-    name = CharField(max_length=64, null=True)
+    name = CharField(max_length=64)
     type = ForeignKey(Type, null=True, on_delete=CASCADE)
     img_name = CharField(max_length=64, null=True)
-    ammount = IntegerField()
-    unit = ForeignKey(Unit, null=True, on_delete=CASCADE)
+    amount = IntegerField(null=True)
+    unit = ForeignKey(Unit,null=True, on_delete=CASCADE)
 
 
 class Fridge(Model):
@@ -24,4 +24,16 @@ class Fridge(Model):
 
 class FridgeProduct(Model):
     fridge = ForeignKey(Fridge, null=True, on_delete=CASCADE)
+    product = ForeignKey(Product, null=True, on_delete=CASCADE)
+
+
+class Recipe(Model):
+    name = CharField(max_length=64, null=True)
+    short_description = CharField(max_length=256, null=True)
+    long_description = CharField(max_length=4096, null=True)
+    img_name = CharField(max_length=64, null=True)
+
+
+class RecipeProduct(Model):
+    recpie = ForeignKey(Recipe, null=True, on_delete=CASCADE)
     product = ForeignKey(Product, null=True, on_delete=CASCADE)
